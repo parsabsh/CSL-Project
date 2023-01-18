26bd3088f43783e3787c02367931a8d6e61f2749
Parsa Bashari <92635013+parsabsh@users.noreply.github.com>
2023-01-18 12:44:58 +0330

    Problem1 differentiation added

---
+++ Problem-01/MIPS/integral_differentiation.asm
@@ -0,0 +1,46 @@
+# Problem 1 - CSL Project
+	.data
+n:	.word 	0
+coef:	.space 	100
+space:	.asciiz " "
+
+	.text 
+main:	li 	$v0,5
+	syscall 
+	sw 	$v0,n
+	add 	$v0,$v0,1
+	add	$t1,$v0,$v0
+	add	$t1,$t1,$t1     # t1 = 4 * (n + 1)
+	li 	$t0,0
+	li 	$v0,6
+	
+inp_lp:	syscall 
+	s.s 	$f0,coef($t0)
+	addi 	$t0,$t0,4
+	blt 	$t0,$t1,inp_lp
+	li	$v0,5
+	syscall 
+	beq 	$v0,1,integ
+# differentiation
+	li 	$t0,0
+	la	$a0,space
+	li	$t5,1
+	mtc1	$t5,$f2
+	cvt.s.w $f3,$f2    # f3 = 1
+	l.s	$f2,n
+	cvt.s.w $f0,$f2    # f0 = n
+	sub	$t1,$t1,4  # t1 = 4 * n
+d_loop:	l.s 	$f1,coef($t0)
+	mul.s 	$f12,$f0,$f1
+	li	$v0,2
+	syscall 
+	li	$v0,4
+	syscall 
+	sub.s  	$f0,$f0,$f3     # f0 = f0 - 1
+	addi 	$t0,$t0,4
+	blt 	$t0,$t1,d_loop
+	b	end
+# integration
+integ:	li	$t1,1
+
+end:	
