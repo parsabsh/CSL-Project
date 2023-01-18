b7b39f3430737f9070a74b5f4b9ee3d0213f1270
Parsa Bashari <92635013+parsabsh@users.noreply.github.com>
2023-01-18 13:22:50 +0330

    Integration added (Problem 1 completed in MIPS)

--- Problem-01/MIPS/integral_differentiation.asm
+++ Problem-01/MIPS/integral_differentiation.asm
@@ -1,8 +1,13 @@
-# Problem 1 - CSL Project
+########################################
+#                                      #
+#        Problem 1 - CSL Project       #
+#                                      #
+########################################
 	.data
 n:	.word 	0
 coef:	.space 	100
 space:	.asciiz " "
+c:	.asciiz "c"
 
 	.text 
 main:	li 	$v0,5
@@ -10,7 +15,7 @@ main:	li 	$v0,5
 	sw 	$v0,n
 	add 	$v0,$v0,1
 	add	$t1,$v0,$v0
-	add	$t1,$t1,$t1     # t1 = 4 * (n + 1)
+	add	$t1,$t1,$t1   # t1 = 4 * (n + 1)
 	li 	$t0,0
 	li 	$v0,6
 	
@@ -21,26 +26,50 @@ inp_lp:	syscall
 	li	$v0,5
 	syscall 
 	beq 	$v0,1,integ
-# differentiation
-	li 	$t0,0
-	la	$a0,space
-	li	$t5,1
-	mtc1	$t5,$f2
-	cvt.s.w $f3,$f2    # f3 = 1
+#################################
+#        Differentiation        #
+#################################
+	jal	init
 	l.s	$f2,n
-	cvt.s.w $f0,$f2    # f0 = n
-	sub	$t1,$t1,4  # t1 = 4 * n
+	cvt.s.w $f0,$f2       # f0 = n
+	sub	$t1,$t1,4     # t1 = 4 * n
 d_loop:	l.s 	$f1,coef($t0)
 	mul.s 	$f12,$f0,$f1
 	li	$v0,2
 	syscall 
 	li	$v0,4
 	syscall 
-	sub.s  	$f0,$f0,$f3     # f0 = f0 - 1
+	sub.s  	$f0,$f0,$f3   # f0 = f0 - 1
 	addi 	$t0,$t0,4
 	blt 	$t0,$t1,d_loop
 	b	end
-# integration
-integ:	li	$t1,1
+#################################
+#          Integration          #
+#################################
+integ:	jal	init
+	l.s	$f2,n
+	cvt.s.w $f0,$f2
+	add.s 	$f0,$f0,$f3   # f0 = n + 1
+i_loop:	l.s 	$f1,coef($t0)
+	div.s 	$f12,$f1,$f0
+	li	$v0,2
+	syscall 
+	li	$v0,4
+	syscall 
+	sub.s  	$f0,$f0,$f3   # f0 = f0 - 1
+	addi 	$t0,$t0,4
+	blt 	$t0,$t1,i_loop
+	la	$a0,c
+	syscall 
+	b	end
+#################################
+#     Some Initializations      #
+#################################
+init:	la	$a0,space
+	li	$t5,1
+	mtc1	$t5,$f2
+	cvt.s.w $f3,$f2       # f3 = 1
+	li 	$t0,0
+	jr	$ra
 
 end:	
