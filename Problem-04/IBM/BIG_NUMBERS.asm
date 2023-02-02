d5969a963ac66c06a5e2d8ad8c90db11720a7940
Parsa Bashari <92635013+parsabsh@users.noreply.github.com>
2023-02-03 01:36:15 +0330

    EDIT COMMENTS

--- Problem-04/IBM/BIG_NUMBERS.asm
+++ Problem-04/IBM/BIG_NUMBERS.asm
@@ -1,16 +1,10 @@
 *********************************
 *				*
-*    Problem 7 - CSL Project    *
+*    Problem 4 - CSL Project    *
 *				*
 *********************************
 * REGISTER USES:
-* R2 = N
-* R3,R4,R5 = TEMP
-* R6 = RETURN ADDRESS
-* R7 = NUMBER - A
-* R10 = NUMBER
-* R11 = A
-PROG7		START		0
+PROG4		START		0
 		STM		14,12,12(13)
 		BALR		12,0
 		USING		*,12
@@ -26,3 +20,4 @@ SECONDUNPACKD	DC		CL80' '
 FIRSTPACKD	DC		CL80' '
 SECONDPACKD	DC		CL80' '
 OPERATOR	DC		CL80' '
+		END		PROG4
