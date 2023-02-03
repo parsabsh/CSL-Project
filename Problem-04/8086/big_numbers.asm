eb37437ff9bac8096d082d51ed3a5937e4b58db6
Parsa Bashari <92635013+parsabsh@users.noreply.github.com>
2023-02-03 19:26:54 +0330

    pr4 8086 started

--- Problem-04/8086/big_numbers.asm
+++ Problem-04/8086/big_numbers.asm
@@ -1,18 +1,103 @@
-        .data
-num1      dw      12345h, 6789h
-num2      dw      9876h, 5432h
-result    dw      0, 0
-        .code
-        mov     bx, 0  ; Initialize carry flag
-add_loop:
-        mov     ax, [num1]
-        add     ax, [num2]
-        adc     bx, 0  ; Add carry flag
-        mov     [result], ax
-        add     num1, 2
-        add     num2, 2
-        add     result, 2
-        cmp     num1, 10h
-        jne     add_loop
-        mov     ax, 4c00h
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                                 ;
+;     Problem 4 - CSL Project     ;
+;                                 ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+pushfgpr   MACRO     ; push flags and general purpose registers
+    pushf
+    push    ax
+    push    bx
+    push    cx
+    push    dx
+ENDM
+
+popfgpr    MACRO     ; pop flags and general purpose registers
+    pop     dx
+    pop     cx
+    pop     bx
+    pop     ax    
+    popf
+ENDM
+
+newline    MACRO    
+    push    ax
+    push    dx
+    mov     ah,9
+    mov     dx,offset newl
+    int     21h
+    pop     dx
+    pop     ax
+ENDM
+
+dataseg SEGMENT 
+first   db      100 dup(0)
+second  db      100 dup(0)
+result  db      100 dup(0)
+press   db      "Press any key to continue... $"
+newl    db      0Dh,0Ah,'$'
+buffer  db      19 dup(0),'$'
+dataseg ENDS
+
+
+codeseg SEGMENT
+        assume  cs:codeseg,ds:dataseg
+        
+        
+start:  mov     ax,dataseg
+        mov     ds,ax
+        call    input
+        mov     ah,1
         int     21h
+        newline
+        cmp     al,'+'
+        je      addlng
+        cmp     al,'-'
+        je      sublng
+cont:   mov     ah,9
+        mov     dx,offset press
+        int     21h
+        mov     ah,1
+        int     21h    
+        mov     ax,4c00h
+        int     21h        
+
+        
+        
+addlng: mov     bx,100     ; result index
+        clc
+adlop:  dec     si
+        dec     di
+        dec     bx
+        cmp     si,0       ; a digit of first number (or zero if index < 0)
+        jl      fzero
+        mov     al,          
+    
+input   PROC          
+        xor     bx,bx
+lop1:   mov     ah,1
+        int     21h
+        cmp     al,0Dh
+        je      endlpi
+        sub     al,'0'
+        mov     first[bx],al
+        inc     bx
+        jmp     lop1
+        mov     si,bx
+        xor     bx,bx
+lop2:   mov     ah,1
+        int     21h
+        cmp     al,0Dh
+        je      endlpi
+        sub     al,'0'
+        mov     second[bx],al
+        inc     bx
+        jmp     lop2
+        mov     di,bx                
+endlpi: ret
+input   ENDP
+
+codeseg ENDS
+
+        end     start
